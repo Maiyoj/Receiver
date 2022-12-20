8aa03eac01afad000dbb0c102fb1fb85f4489ddc
Maiyoj <valentinemaiyo1995@gmail.com>
2022-12-20 12:26:38 +0300

    update chnages to the API

--- config/database.php
+++ config/database.php
@@ -1,26 +1,19 @@
-<?php
-  class dataBase{
-  // specify your own database credentials
-  private $host = "localhost";
-  private $db_name = "api_db";
-  private $username = "root";
-  private $password = "";
-  public $conn;
-  
-  // get the database connection
-  public function getConnection(){
-   
-    $this->conn = null;
-
-    try{
-        $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-    }catch(PDOException $exception){
-        echo "Connection error: " . $exception->getMessage();
-    }
-
-    return $this->conn;
-}
-  }
-
-
+<?php 
+    class Database {
+        private $host = "localhost";
+        private $database_name = "api_db";
+        private $username = "root";
+        private $password = "";
+        public $conn;
+        public function getConnection(){
+            $this->conn = null;
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
+                $this->conn->exec("set names utf8");
+            }catch(PDOException $exception){
+                echo "Database could not be connected: " . $exception->getMessage();
+            }
+            return $this->conn;
+        }
+    }  
 ?>

4fd4eedaf18659a71ee3c01892e97c07d49a8e78
Maiyoj <valentinemaiyo1995@gmail.com>
2022-12-20 08:30:50 +0300

    Intial commit

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+  class dataBase{
+  // specify your own database credentials
+  private $host = "localhost";
+  private $db_name = "api_db";
+  private $username = "root";
+  private $password = "";
+  public $conn;
+ 
+  }
+
+
+?>
